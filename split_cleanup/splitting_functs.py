ed28854711adc14c13827f389195250f1f4d4eed
niting <niting1209@gmail.com>
2023-09-07 16:41:46 -0400

    Initial fix for Act labels in volumes which have Act labels in their marginalia

--- split_cleanup/splitting_functs.py
+++ split_cleanup/splitting_functs.py
@@ -3,7 +3,7 @@ import pandas as pd
 from string import punctuation
 import nltk
 from nltk.metrics import edit_distance
-# from nltk import word_tokenize
+import re
 
 nltk.download('words', quiet=True)
 
@@ -385,6 +385,31 @@ def getImgsPath(df, fileType, dir_imgs):
 
 # Functions for word corrections...
 def correct_words_batch(sentences, target_words, threshold):
+    """
+    Correct some incorrect words in `sentences`. 
+    Words which are in `target_words` are checked in `sentences`
+    and corrected.
+    The correction is based on a Levenshtein distance. A lower limit 
+    of 0 and an upper limit of `threshold` is used.
+
+    Parameters
+    ----------
+    sentences : Pandas.Series
+        A series that will be checked and corrected.
+    target_words: list
+        List of Str words to correct.
+    threshold: float
+        An upper threshold to check and compare the word 
+        in `target_words` with the words in `chunk`.
+
+    Returns
+    -------
+    tuple of (list, list, list)
+        Lists of corrected sentences, 
+        flags for each sentence indicating whether a correction was made, 
+        and lists of original (incorrect) words for those corrected sentences.
+    """
+
     punct = set(punctuation)
     
     corrected_sentences = []
@@ -424,7 +449,28 @@ def correct_words_batch(sentences, target_words, threshold):
     return corrected_sentences, flags, org_words
 
 
-def process_chunk(chunk, spell, target_words, threshold):
+def correct_chunk(chunk, target_words, threshold):
+    """
+    Search and correct words in `chunk` DataFrame's 'sentence' column
+    using Levenshtein distance.
+
+    Parameters
+    ----------
+    chunk : Pandas.DataFrame
+        DataFrame that needs to be processed. Must have a 'sentence' column.
+    target_words: list
+        List of Str words to correct.
+    threshold: float
+        An upper threshold to check and compare the word 
+        in `target_words` with the words in `chunk`.
+
+    Returns
+    -------
+    Pandas DataFrame
+        A modified version of `chunk` which includes a corrected sentence, flag,
+        and an original words column.
+    """
+    
     corrected_sentences_batch, flags_batch, org_words_batch = correct_words_batch(chunk['sentence'], target_words, threshold)
     corrected_df = pd.DataFrame({'corrected_sentence': corrected_sentences_batch, 'flag': flags_batch, 'org_words': org_words_batch})
 
@@ -511,3 +557,59 @@ def addJoints(sentence):
         Either "Joint Resolution" or "Act" based on `sentence`.
     """
     return 'Joint Resolution' if 'joint' in sentence.lower().split()[:3] and 'resolution' in sentence.lower().split()[:4] else 'Act'
+
+
+def process_row(row, pattern, cols):
+    """
+    Find matches in the 'sentence' column of the given row.
+    If 2 or more matches found, then a list of 2 rows 
+    split on the 2nd match are returned. Otherwise, the 
+    original row is returned in a list.
+    
+    Parameters
+    ----------
+    row : Pandas DataFrame row
+        The row of a the Pandas dataframe which should atleast 
+        contain the 'sentence' column.
+    pattern: re.compile
+        A Regex pattern.
+    cols: list
+        A list of the DataFrame's columns.
+
+    Returns
+    -------
+    list
+        A list of either the original row or 
+        two rows split on the 2nd match of `pattern`.
+    """
+
+    # Find all matches in the text
+    matches = re.finditer(pattern, row['sentence'], re.IGNORECASE)
+
+    # Initialize variables to keep track of the start and end indices of the 2nd match
+    start_idx = None
+    end_idx = None
+    
+    # Iterate through the matches and store the start and end indices of the second match
+    for i, match in enumerate(matches):
+        if i == 1:
+            start_idx, end_idx = match.span()
+        elif i > 1:
+            # If there's a third match, break to stop further processing
+            break
+
+    if start_idx is not None and end_idx is not None:
+        split_result = [row['sentence'][:start_idx], row['sentence'][end_idx:]]
+    else:
+        split_result = [row['sentence']]
+
+    # Create new rows dynamically with all columns
+    new_rows = []
+    for i in range(len(split_result)):
+        new_row = {}
+        for col in cols:
+            new_row[col] = row[col]
+        new_row['sentence'] = split_result[i]
+        new_rows.append(new_row)
+
+    return new_rows
